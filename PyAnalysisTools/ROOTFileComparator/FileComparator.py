f710ba44115c77b2d65b2221506ae08fd149b7dc
morgenst <marcus.matthias.morgenstern@cern.ch>
2016-05-28 00:16:35 +0200

    merge with master

--- PyAnalysisTools/ROOTFileComparator/FileComparator.py
+++ PyAnalysisTools/ROOTFileComparator/FileComparator.py
@@ -5,8 +5,8 @@ from ROOT import TFile
 
 class FileComparator(object):
     def __init__(self, args):
-        self.lhs = TFile.Open(args.lhs, 'READ')
-        self.rhs = TFile.Open(args.rhs, 'READ')
+        self.lhs = args.lhs
+        self.rhs = args.rhs
         self.outdir = args.outdir
 
     def __del__(self):
