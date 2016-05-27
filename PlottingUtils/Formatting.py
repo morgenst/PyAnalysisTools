f49b9dd2fcc25c15fb466d279135677647466eba
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-05-27 15:24:18 +0200

    implement rebinning

--- PlottingUtils/Formatting.py
+++ PlottingUtils/Formatting.py
@@ -1,8 +1,10 @@
 __author__ = 'marcusmorgenstern'
 __mail__ = ''
 
+import numpy as np
+import re
 import ROOT
-from base import InvalidInputError
+from base import InvalidInputError, _logger
 
 
 def set_title_x(obj, title):
@@ -31,5 +33,72 @@ def set_style_options(obj, style):
             try:
                 getattr(obj, "Set" + style_object.capitalize() + style_option.capitalize())(value)
             except AttributeError:
-                pass
-                #_logger
+                _logger.warning("Could not set rquested style " + style_object.capitalize() + style_option.capitalize()
+                                + " for object " + str(obj))
+
+
+def make_text(x, y, text, size=0.05, angle=0, font=42, color=ROOT.kBlack, NDC=True):
+    t = ROOT.TLatex(x, y, text)
+    t.SetTextSize(size)
+    t.SetTextAngle(angle)
+    t.SetTextFont(font)
+    t.SetTextColor(color)
+    t.SetNDC()
+    return t
+
+
+def rebin(self, histograms, factor=None):
+    if factor is None:
+        return histograms
+    if type(histograms) == dict:
+        for key, hist in histograms.items():
+            try:
+                histograms[key].append(_rebin_hist(hist, factor))
+            except KeyError:
+                histograms[key] = [_rebin_hist(hist, factor)]
+        else:
+            raise InvalidInputError('Invalid binning: ' + str(factor))
+    else:
+        histograms = _rebin_hist(histograms, factor)
+    return histograms
+
+
+def _rebin_hist(hist, factor):
+    if type(factor) is int:
+        hist = _rebin_1d_hist(hist, factor)
+    elif type(factor) == list:
+        binning = np.array('d', factor)
+        _logger.debug('rebin histogram %s asymmetrically' % (hist.GetName()))
+        hist = __rebin_asymmetric_1d_hist(hist, len(factor), binning)
+    else:
+        raise InvalidInputError('Invalid binning: ' + str(factor))
+    return hist
+
+
+def _rebin_1d_hist(hist, factor):
+    y_title = hist.GetYaxis().GetTitle()
+    if not factor:
+        return
+    try:
+        try:
+            binning = float(re.findall('[0-9].+', y_title)[0])
+        except ValueError:
+            binning = int(re.findall('[0-9]+', y_title)[0])
+        y_title = y_title.replace(str(binning), str(binning * factor))
+    except IndexError, KeyError:
+        pass
+    hist.GetYaxis().SetTitle(y_title)
+    return hist.Rebin(factor)
+
+
+def __rebin_asymmetric_1d_hist(hist, n_bins, bins):
+    hist.GetYaxis().SetTitle(hist.GetYaxis().GetTitle() + ' x %i' % n_bins)
+    return hist.Rebin(n_bins - 1, hist.GetName(), bins)
+
+
+def merge_overflow_bins(self, hists, xmax=None):
+    if type(hists) == dict:
+        for item in hists.values():
+            self.__mergeOverflowBinsTH1F(item, xmax)
+    else:
+        self.__mergeOverflowBinsTH1F(hists, xmax)
