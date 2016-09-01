edb3cb0a56edced794de4f464b544f08b554380e
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-09-01 22:29:56 +0200

    implement significance calculation

---
+++ PyAnalysisTools/AnalysisTools/StatisticsTools.py
@@ -0,0 +1,34 @@
+from math import sqrt
+from PyAnalysisTools.base import _logger, InvalidInputError
+import PyAnalysisTools.PlottingUtils.Formatting as FM
+import PyAnalysisTools.PlottingUtils.PlottingTools as PT
+
+
+def consistency_check_bins(obj1, obj2):
+    return obj1.GetNbinsX() == obj2.GetNbinsX()
+
+
+def calculate_significance(signal, background):
+    try:
+        return float(signal)/sqrt(float(signal) + float(background))
+    except ZeroDivisionError:
+        return 0.
+
+
+def get_significance(signal, background):
+    _logger.error("Not implemented yet. Uses just bin contents rather than integrals")
+    significance_hist = signal.Clone("significance")
+    if not consistency_check_bins(signal, background):
+        _logger.error("Signal and background have different binnings.")
+        raise InvalidInputError("Inconsistent binning")
+    for ibin in range(signal.GetNbinsX() + 1):
+        significance_hist.SetBinContent(ibin, calculate_significance(signal.Integral(-1,ibin),
+                                                                     background.Integral(-1,ibin)))
+    FM.set_title_y(significance_hist, "S/#sqrt(S + B)")
+    canvas = PT.retrieve_new_canvas("significance", "")
+    canvas.cd()
+    #todo: call addHistogram to canvas (see MMPP-160)
+    significance_hist.SetLineColor(2)
+    significance_hist.SetFillColor(0)
+    significance_hist.Draw("l")
+    return canvas
