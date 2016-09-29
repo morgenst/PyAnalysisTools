321fdb8b46f0fe15b487f9cee1867d007405909e
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-09-29 22:42:21 +0200

    add synchronisation script

---
+++ run_scripts/synchronise.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+import sys
+
+from subprocess import call
+from PyAnalysisTools.base.YAMLHandle import YAMLLoader
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="Synchronisation script")
+    parser.add_argument("path", type=str, help="input path")
+    parser.add_argument("--config", "-c", type=str, required=True, help="configuration")
+
+    args = parser.parse_args()
+
+    config = YAMLLoader.read_yaml(args.config)
+    for data in config["content"]:
+        cmd = ["rsync", "-ravu", os.path.join(args.path, data, "."), " %s@%s:%s" % (config["user"],
+                                                                                    config["server"],
+                                                                                    os.path.join(config["destination"], data))]
+        print "executing " + ' '.join(cmd)
+        os.system(" ".join(cmd))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
