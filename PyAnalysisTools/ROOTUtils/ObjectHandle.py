50647e0c5033fb397d0acd5440427c6002bab510
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-10-03 14:51:16 +0200

    implement merging by process

--- PyAnalysisTools/ROOTUtils/ObjectHandle.py
+++ PyAnalysisTools/ROOTUtils/ObjectHandle.py
@@ -15,3 +15,18 @@ def get_objects_from_canvas_by_type(canvas, typename):
     obj = get_objects_from_canvas(canvas)
     obj = filter(lambda o: o.InheritsFrom(typename), obj)
     return obj
+
+
+def merge_objects_by_process_type(canvas, process_config, merge_type):
+    objects = get_objects_from_canvas_by_type(canvas, "TH1")
+    if len(objects) == 0:
+        return None
+    first_object = objects[0]
+    variable = "_".join(first_object.GetName().split("_")[0:-1])
+    merged_hist = first_object.Clone("_".join([variable, merge_type]))
+    for obj in objects:
+        process_name = obj.GetName().split("_")[-1]
+        if not process_config[process_name].type == merge_type:
+            continue
+        merged_hist.Add(obj)
+    return merged_hist
