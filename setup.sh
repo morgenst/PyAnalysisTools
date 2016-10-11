d0ca7349a3c8f0e1e08c6218a8d85b12da5f23b3
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-10-11 15:16:10 +0200

    add lsetup pytools (for numpy) on lxplus/romanescu/stoomboot

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+if [[ $(hostname -s) = *lxplus* ]] || [[ $(hostname -s) = *stbc-* ]] || [[ $(hostname -s) == *romanescu* ]]; then
+    lsetup "sft releases/pytools/1.9_python2.7-5c0ab"
+fi
+
 export PYTHONPATH=${PWD}:${PYTHONPATH}
