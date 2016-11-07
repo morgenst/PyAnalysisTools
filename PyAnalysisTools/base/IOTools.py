9b64984a8fb812913160bfe16ce4804e6725a834
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-11-07 17:37:34 +0100

    implement file size sensitive merging logic

--- PyAnalysisTools/base/IOTools.py
+++ PyAnalysisTools/base/IOTools.py
@@ -1,5 +1,7 @@
 import ROOT
 import os
+from subprocess import check_call
+from PyAnalysisTools.base.ShellUtils import move, remove_directory, make_dirs
 
 
 class Writer:
@@ -64,3 +66,53 @@ class Writer:
     def set_directory(self, directory):
         self.__check_and_create_directory(directory)
         self.dir = directory
+
+
+def merge_files(input_file_list, output_path, prefix, merge_dir=None, force=False):
+    def build_buckets(file_list):
+        limit = 2. * 1024. * 1024. * 1024.
+        if sum(map(os.path.getsize, file_list)) < limit:
+            return file_list
+        bucket_list = []
+        tmp = []
+        summed_file_size = 0.
+        for file_name in file_list:
+            if summed_file_size > limit:
+                summed_file_size = 0.
+                bucket_list.append(tmp)
+                tmp = []
+            summed_file_size += os.path.getsize(file_name)
+            tmp.append(file_name)
+        bucket_list.append(tmp)
+        return bucket_list
+
+    def merge(file_lists, prefix, output_path, merge_dir=None, force=False):
+        if len([f for chunk in file_lists for f in chunk]) == 0:
+            return
+        for file_list in file_lists:
+            merge_cmd = "hadd "
+            if force:
+                merge_cmd += " -f "
+            output_file_name = "{:s}_{:d}.root".format(prefix, file_lists.index(file_list))
+            merge_cmd += "%s %s" % (output_file_name, " ".join(file_list))
+            if not force and os.path.exists(os.path.join(output_path, output_file_name)):
+                continue
+            check_call(merge_cmd.split())
+            if not merge_dir == output_path:
+                move(os.path.join(merge_dir, output_file_name), os.path.join(output_path, output_file_name))
+
+    def setup_paths(output_path, merge_dir):
+        if not os.path.exists(output_path):
+            make_dirs(output_path)
+        if merge_dir is None:
+            merge_dir = output_path
+        else:
+            merge_dir = os.path.abspath(merge_dir)
+            make_dirs(merge_dir)
+        os.chdir(merge_dir)
+
+    buckets = build_buckets(input_file_list)
+    setup_paths(output_path, merge_dir)
+    merge(buckets, prefix, output_path, merge_dir, force)
+    if merge_dir is not None:
+        remove_directory(os.path.abspath(merge_dir))
