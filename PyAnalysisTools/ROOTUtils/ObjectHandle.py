542196d17d4210f53a1f357c4e78077c6c1f01d0
morgenst <marcus.matthias.morgenstern@gmail.com>
2017-01-06 09:18:00 +0100

    extend get_objects_from_canvas_by_name to support list of names

--- PyAnalysisTools/ROOTUtils/ObjectHandle.py
+++ PyAnalysisTools/ROOTUtils/ObjectHandle.py
@@ -1,4 +1,5 @@
-# helper methods to deal with objects
+import re
+from PyAnalysisTools.base import _logger
 
 
 def get_objects_from_canvas(canvas):
@@ -11,6 +12,17 @@ def get_objects_from_canvas_by_type(canvas, typename):
     obj = get_objects_from_canvas(canvas)
     obj = filter(lambda o: o.InheritsFrom(typename), obj)
     return obj
+
+
+def get_objects_from_canvas_by_name(canvas, name):
+    objects = get_objects_from_canvas(canvas)
+    if not isinstance(name, list):
+        name = [name]
+    patterns = map(re.compile, name)
+    objects = filter(lambda obj: any(re.search(pattern, obj.GetName()) for pattern in patterns), objects)
+    if len(objects) == 0:
+        return None
+    return objects
 
 
 def merge_objects_by_process_type(canvas, process_config, merge_type):
@@ -26,3 +38,14 @@ def merge_objects_by_process_type(canvas, process_config, merge_type):
             continue
         merged_hist.Add(obj)
     return merged_hist
+
+
+def find_branches_matching_pattern(tree, pattern):
+    pattern = re.compile(pattern)
+    branch_names = []
+    for branch in tree.GetListOfBranches():
+        if re.search(pattern, branch.GetName()):
+            branch_names.append(branch.GetName())
+    if len(branch_names) == 0:
+        _logger.warning("Could not find objects matching %s in %s" % (pattern, tree.GetName()))
+    return branch_names
