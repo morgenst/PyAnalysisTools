5b023baa9cfc48ba9766365d23f5ec485568ccaf
morgens <marcus.matthias.morgenstern@cern.ch>
2017-07-06 13:55:47 +0200

    bugfix rebin of THStack

--- PyAnalysisTools/PlottingUtils/PlottingTools.py
+++ PyAnalysisTools/PlottingUtils/PlottingTools.py
@@ -141,7 +141,7 @@ def format_hist(hist, plot_config):
             hist.GetZaxis().SetTitle(plot_config.ztitle)
         if hasattr(plot_config, "rebinX") and hasattr(plot_config.rebinY):
             hist = HT.rebin2D(hist, plot_config.rebinX, plot_config.rebinY)
-    if hasattr(plot_config, "rebin"):
+    if hasattr(plot_config, "rebin") and not isinstance(hist, ROOT.THStack):
         hist = HT.rebin(hist, plot_config.rebin)
         yscale = 1.1
         if hasattr(plot_config, "yscale"):
