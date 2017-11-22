1b40833b2c0207a101b00cdb962cc521cbd93239
morgens <marcus.matthias.morgenstern@cern.ch>
2017-11-22 02:22:57 +0100

    implement support for single run

--- PyAnalysisTools/AnalysisTools/Utilities.py
+++ PyAnalysisTools/AnalysisTools/Utilities.py
@@ -4,6 +4,8 @@ def find_data_period(run_number, data_summary):
     for year_tag, summary in data_summary.iteritems():
         for period, run_ranges in summary.iteritems():
             for run_range in run_ranges:
+                if isinstance(run_range, int):
+                    run_range = [run_range]
                 if min(run_range) <= run_number <= max(run_range):
                     return period, year_tag
     return None, None
