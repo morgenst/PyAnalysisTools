60774a6a514d85c386b6cdfa48cf9fc24acd388f
morgens <marcus.matthias.morgenstern@cern.ch>
2018-03-07 20:31:20 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(name='PyAnalysisTools',
+      version='0.1',
+      description='PyAnalysisTools',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=find_packages(),#packages=['PyAnalysisTools'],
+      zip_safe=False)
