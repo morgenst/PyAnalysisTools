91158c76272685a431b5e9072017615bc12d5734
morgens <marcus.matthias.morgenstern@cern.ch>
2018-07-10 09:28:53 +0200

    update POI range

--- PyAnalysisTools/AnalysisTools/HistFitterWrapper.py
+++ PyAnalysisTools/AnalysisTools/HistFitterWrapper.py
@@ -536,7 +536,7 @@ class HistFitterCountingExperiment(HistFitterWrapper):
         # bkgSample.addSystematic(ucb)
 
         sigSample = Sample(kwargs["sig_name"], kPink)
-        sigSample.setNormFactor("mu_Sig", 1., 0., 1.)
+        sigSample.setNormFactor("mu_Sig", 1., 0., 100.)
         sigSample.setStatConfig(True)
         # sigSample.setNormByTheory()
         print nsig
