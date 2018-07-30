49b2ca6f39755537419dbc931e731302c1a5c5af
morgens <marcus.matthias.morgenstern@cern.ch>
2018-07-30 12:34:32 +0200

    update formatting option
    Relates to MMPP-1410

--- run_scripts/print_dataset_list.py
+++ run_scripts/print_dataset_list.py
@@ -9,7 +9,7 @@ def main(argv):
     parser.add_argument("dataset_list", type=str, help="dataset list input file")
     parser.add_argument("xs_info_file", type=str, help="cross section input file")
     parser.add_argument("--format", "-f", type=str, choices=map(str, tabulate_formats), default="plain",
-                        help="output format {:s}".format(DatasetPrinter.get_supported_formats()))
+                        help="output format")
     args = parser.parse_args()
     printer = DatasetPrinter(**vars(args))
     printer.pprint()
