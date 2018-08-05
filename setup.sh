28ab4a462885b12a4e55638a9aab10b2e2f59c1d
morgens <marcus.matthias.morgenstern@cern.ch>
2018-08-05 16:23:51 +0200

    path independent setup
    Resolve ELB-9

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ if [[ $(hostname -s) = *lxplus* ]] || [[ $(hostname -s) == *romanescu* ]] || [[ 
     export PYTHONPATH=~/pythonmodules/lib/python2.7/site-packages/:$PYTHONPATH
 fi
 
-export PYTHONPATH=${PWD}:${PYTHONPATH}
+called=$_
+[[ $called != $0 ]]
+
+export PYTHONPATH=`dirname "${BASH_SOURCE[@]}"`:${PYTHONPATH}
+export PATH=`dirname "${BASH_SOURCE[@]}"`/run_scripts:${PATH}
