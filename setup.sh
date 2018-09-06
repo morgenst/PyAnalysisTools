68150881fac12790b666ddd3c9a3e6dcab25ae6a
morgens <marcus.matthias.morgenstern@cern.ch>
2018-09-06 12:24:28 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,18 +1,15 @@
-#!/usr/bin/env bash
+#####!/usr/bin/env bash
 
 if [[ $(hostname -s) = *lxplus* ]] || [[ $(hostname -s) == *romanescu* ]] || [[ $(hostname -s) = *pc-tbed-pub-* ]]; then
     export PYTHONPATH=~/pythonmodules/lib/python2.7/site-packages/:$PYTHONPATH
 fi
 
-if [[ $(hostname -s) = *lxplus* ]] || [[ $(hostname -s) == *romanescu* ]] || [[ $(hostname -s) = *pc-tbed-pub-* ]]; then
-    CWD=`dirname $(readlink -f "$0")`
-elif [[ $(hostname -s) = *stbc-* ]]; then
+if [[ $(hostname -s) = *lxplus* ]] || [[ $(hostname -s) == *romanescu* ]] || [[ $(hostname -s) = *pc-tbed-pub-* ]] || [[ $(hostname -s) = *stbc-* ]]; then
     CWD=`dirname $(readlink -f "${BASH_SOURCE[0]}")`
 else
     called=$_
     [[ $called != $0 ]]
     CWD=`dirname "${BASH_SOURCE[@]}"`
 fi
-
 export PYTHONPATH=${CWD}:${PYTHONPATH}
 export PATH=${CWD}/run_scripts:${PATH}
