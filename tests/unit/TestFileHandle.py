0f05577119b8f9c53e42888b7e3e8ff087a91221
morgens <marcus.matthias.morgenstern@cern.ch>
2019-09-23 10:33:34 +0200

    add more test cases for file handle

--- tests/unit/TestFileHandle.py
+++ tests/unit/TestFileHandle.py
@@ -1,49 +1,90 @@
 import unittest
-
+import os
+import ROOT
 from PyAnalysisTools.ROOTUtils.FileHandle import FileHandle
 
 
 class TestFileHandle(unittest.TestCase):
     def setUp(self):
-        self.handle = FileHandle(file_name='test.root')
+        self.file_name = os.path.join(os.path.dirname(__file__), 'fixtures/files/ntuple-311331_0.MC16d.root')
+        self.file_handle = FileHandle(file_name=self.file_name)
+        
+    def tearDown(self):
+        del self.file_handle
 
     @unittest.skip("Not correctly implemented")
     def testFileOpenNoPathSuccess(self):
         handle = FileHandle(file_name='test.root')
         handle.open()
 
-    @unittest.skip("Not correctly implemented")
     def testFileOpenNoPathFail(self):
         with self.assertRaises(ValueError):
             FileHandle(file_name='NonExistingFile.root').open()
 
-    @unittest.skip("Not correctly implemented")
     def testFileGetObjects(self):
-        self.handle.open()
-        l = self.handle.get_objects()
-        self.assertEqual(len(l), 1)
+        self.file_handle.open()
+        l = self.file_handle.get_objects()
+        self.assertGreater(len(l), 1)
 
-    @unittest.skip("Not correctly implemented")
     def testFileGetObjectsByTypeCanvas(self):
-        self.handle.open()
-        l = self.handle.get_objects_by_type("TCanvas")
+        l = self.file_handle.get_objects_by_type("TCanvas")
         self.assertEqual(len(l), 0)
 
-    @unittest.skip("Not correctly implemented")
     def testFileGetObjectByTypeHist(self):
-        self.handle.open()
-        l = self.handle.get_objects_by_type("TH1F")
+        l = self.file_handle.get_objects_by_type("TH1D")
+        self.assertGreater(len(l), 1)
+
+    def testFileGetObjectByNameExisting(self):
+        obj = self.file_handle.get_object_by_name("Nominal")
+        self.assertIsInstance(obj, ROOT.TDirectoryFile)
+
+    def test_get_object_inexisting_with_tdir(self):
+        obj = self.file_handle.get_object_by_name('BaseSelection_lq_tree_syst_Final', "Nominal")
+        self.assertIsInstance(obj, ROOT.TTree)
+
+    def test_get_object_inexisting(self):
+        with self.assertRaises(ValueError):
+            self.file_handle.get_object_by_name("SomeNonExistingObj")
+
+    def test_get_object_inexisting_tdir(self):
+        with self.assertRaises(ValueError):
+            self.file_handle.get_object_by_name('BaseSelection_lq_tree_syst_Final', "Nominal2")
+
+    def test_branch_names(self):
+        l = self.file_handle.get_branch_names_from_tree('BaseSelection_lq_tree_syst_Final', 'Nominal')
+        self.assertGreater(len(l), 1)
+
+    def test_get_objects_by_pattern(self):
+        l = self.file_handle.get_objects_by_pattern('JET_')
+        self.assertGreater(len(l), 1)
+
+    def test_get_objects_by_pattern_inexisting(self):
+        l = self.file_handle.get_objects_by_pattern('BJET_')
+        self.assertEqual(len(l), 0)
+
+    def test_branch_names_pattern(self):
+        l = self.file_handle.get_branch_names_from_tree('BaseSelection_lq_tree_syst_Final', 'Nominal', 'muon_n')
         self.assertEqual(len(l), 1)
 
-    @unittest.skip("Not correctly implemented")
-    def testFileGetObjectByNameExisting(self):
-        self.handle.open()
-        obj = self.handle.get_object_by_name("test_hist_1")
-        self.assertTrue(obj is not None)
+    def test_branch_names_pattern_inexisting(self):
+        l = self.file_handle.get_branch_names_from_tree('BaseSelection_lq_tree_syst_Final', 'Nominal', 'muon_n2')
+        self.assertEqual(len(l), 0)
 
-    @unittest.skip("Not correctly implemented")
-    def testFileGetObjectByNameNonExisting(self):
-        self.handle.open()
+    def test_branch_names_pattern_inexisting_tree(self):
         with self.assertRaises(ValueError):
-            obj = self.handle.get_object_by_name("SomeNonExistingObj")
+            self.file_handle.get_branch_names_from_tree('tree', 'Nominal', 'muon_n2')
+
+    def test_get_number_of_total_events(self):
+        self.assertEqual(self.file_handle.get_number_of_total_events(), 10000.)
+
+    def test_get_number_of_total_events_uneweighted(self):
+        self.assertEqual(self.file_handle.get_number_of_total_events(unweighted=True), 10000.)
+
+    def test_get_daod_events(self):
+        self.assertEqual(self.file_handle.get_daod_events(), 10000.)
+
+    def test_friend(self):
+        file_handle = FileHandle(file_name=self.file_name,
+                                 friend_directory=os.path.join(os.path.dirname(__file__), 'fixtures/files/'),
+                                 friend_pattern='ntuple-', friend_tree_names='BaseSelection_lq_tree_syst_Final')
 
