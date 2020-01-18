bd2456eaad3db178536708b095fe092e78f169ee
morgens <marcus.matthias.morgenstern@cern.ch>
2020-01-18 14:39:51 +0100

    enable fake fs

--- tests/unit/TestUtilities.py
+++ tests/unit/TestUtilities.py
@@ -1,19 +1,16 @@
-# import time
-import unittest
 import os
 from PyAnalysisTools.base import Utilities, InvalidInputError
-# from pyfakefs.fake_filesystem_unittest import TestCase
+from pyfakefs.fake_filesystem_unittest import TestCase
 
 cwd = os.path.dirname(__file__)
 
 
-class TestUtilities(unittest.TestCase):
+class TestUtilities(TestCase):
     def setUp(self):
-        pass
-        # self.setUpPyfakefs()
+        self.setUpPyfakefs()
 
     def tearDown(self):
-        pass
+        self.tearDownPyfakefs()
 
     def test_merge_dicts(self):
         d1 = {'foo': 1}
@@ -35,7 +32,6 @@ class TestUtilities(unittest.TestCase):
     def test_check_required_args_missing(self):
         self.assertEqual('arg', Utilities.check_required_args('arg', foo=1))
 
-    @unittest.skip("Need MMPP-1841")
     def test_cleaner_check_lifetime(self):
         self.fs.create_file('/foo/bar.txt')
         self.assertTrue(Utilities.Cleaner.check_lifetime(100, 'foo', ['bar.txt']))
@@ -46,17 +42,15 @@ class TestUtilities(unittest.TestCase):
     def test_flatten_more_elements(self):
         self.assertEqual(['foo/bar/1', 'foo/bar/2'], Utilities.flatten({'foo': {'bar': ["1", "2"]}}))
 
-    @unittest.skip("Requires fake fs")
     def test_cleaner_default_ctor(self):
         cleaner = Utilities.Cleaner(base_path='foo')
         self.assertTrue(cleaner.safe)
-        self.assertEqual('foo', cleaner.base_path)
+        self.assertEqual('/foo', cleaner.base_path)
         self.assertEqual([".git", ".keep", ".svn", "InstallArea", "RootCoreBin", "WorkArea"], cleaner.keep_pattern)
         self.assertEqual([], cleaner.deletion_list)
         self.assertEqual(14., cleaner.touch_threshold_days)
         self.assertEqual(None, cleaner.trash_path)
 
-    @unittest.skip("Requires fake fs")
     def test_cleaner_default_ctor_trash(self):
         cleaner = Utilities.Cleaner(base_path='foo', trash_path='bar')
         self.assertEqual('bar', cleaner.trash_path)
